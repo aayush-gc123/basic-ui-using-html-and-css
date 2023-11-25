28ad97d4f59e3e74b8b6f0a6cb6a58c0a8777863
aayush-gc123 <aayushgc23@gmail.com>
2023-11-25 19:59:58 +0545

    aading 2 files

---
+++ ss.js
@@ -0,0 +1,24 @@
+const arr=[1,2,3,4,5,6,7]
+// const y=arr.forEach(function(val){
+//     console.log(val)
+// })
+// const y=arr.map((val)=>{
+// return val
+// })
+// console.log(y)
+
+
+// const s=arr.filter((x)=>{
+// return x>3 && x<7
+
+// })
+// console.log(s)
+
+
+
+// const s=arr.reduce((acc,cv)=>{
+//     return acc+cv;
+
+// })
+// console.log(s)
+
